e83a904225d2defe70ad2460abb1d4c65993700c
Elieo Chavez <gnuchavez@gmail.com>
2022-11-13 18:32:54 -0600

    Update: Herencia de Templates y URL dinamicas

--- appcoder/urls.py
+++ appcoder/urls.py
@@ -4,9 +4,9 @@ from appcoder.views import * #Importamos todas las vistas
 
 #El archivo debe tener un archivo llamado exactamente urlpattern
 urlpatterns = [
-    path("",inicio),
-    path("estudiantes/",estudiantes),
-    path("profesores/",profesores),
-    path("cursos/",cursos),
-    path("entregables/",entregables),
+    path("inicio/",inicio, name="coder-inicio"),
+    path("estudiantes/",estudiantes, name="coder-estudiantes"),
+    path("profesores/",profesores, name="coder-profesores"),
+    path("cursos/",cursos, name="coder-cursos"),
+    path("entregables/",entregables, name="coder-entregables"),
 ]
